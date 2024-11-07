e311b88b8bf755ea6a46f7388812be434249a823
Giorgi Bebia <139773975+GiorgiBebia@users.noreply.github.com>
2024-11-07 20:09:20 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Advance_Generator_App/",
+  base: "/Advice_Generator_App/",
   plugins: [react()],
 });
